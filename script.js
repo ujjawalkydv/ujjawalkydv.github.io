45e1a74a7a6a44ecf4e4bb8a29b4fa2d723c7a15
Ujjawal <ujjawalvirus@gmail.com>
2022-11-25 02:10:02 +0530

    master

---
+++ script.js
@@ -0,0 +1,8 @@
+// {/* <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script> */}
+// var typed = new Typed('.type', {
+//     strings: ['Learner', 'Coder', 'Ex-Electrical Engineer'],
+//     typeSpeed: 20,
+//     backSpeed: 20,
+//     loop: true,
+//     loopCount: Infinity
+//   });
