2444d01d19ff59489587511c51859471673d62e0
Ujjawal <ujjawalvirus@gmail.com>
2022-11-25 17:34:41 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// {/* <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script> */}
-// var typed = new Typed('.type', {
-//     strings: ['Learner', 'Coder', 'Ex-Electrical Engineer'],
-//     typeSpeed: 20,
-//     backSpeed: 20,
-//     loop: true,
-//     loopCount: Infinity
-//   });
+// type.js Script
+
+var typed = new Typed('.type', {
+    strings: ['Learner', 'Coder', 'Ex-Electrical Engineer'],
+    typeSpeed: 20,
+    backSpeed: 20,
+    loop: true,
+    loopCount: Infinity
+  });
+
